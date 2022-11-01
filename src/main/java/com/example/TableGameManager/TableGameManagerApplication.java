427d22056dd8ee5ee7e063147a6e8a1849de19ff
abenitez <abraham.benitez.1994@gmail.com>
2022-11-01 11:20:47 +0100

    Spring Pproject

---
+++ src/main/java/com/example/TableGameManager/TableGameManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TableGameManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableGameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableGameManagerApplication.class, args);
+	}
+
+}
